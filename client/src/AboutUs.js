5c949b80613ee59b3cbd58a9f96b95f0522a6fd9
rogers.machara <rogersmachara13@gmail.com>
2024-08-07 19:54:24 +0300

    about article

--- client/src/AboutUs.js
+++ client/src/AboutUs.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import NavBar from './components/NavBar'
 import AidGallery from './components/AidGallery'
+import AboutArticle from './components/AboutArticle'
 
 const AboutUs = () => {
   return (
     <div>
-      <NavBar/>
+      <NavBar />
       <h2>Let's create decent work opportunities for all.</h2>
-      <AidGallery/>
+      <AidGallery />
+      <AboutArticle />
     </div>
   )
 }
---
+++ client/src/components/AboutArticle.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../AboutArticle.css'; 
+
+const AboutArticle = () => {
+  return (
+    <div className="abt-article">
+      <h2 className="abt-title">About Us</h2>
+      <p className="abt-content">
+        Welcome to our company. We are dedicated to providing the best service possible. Our team works tirelessly to ensure customer satisfaction. We value integrity, excellence, and innovation.
+      </p>
+    </div>
+  );
+};
+
+export default AboutArticle;
