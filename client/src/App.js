cdaf10289f6f01d1598b62668113a346475d0160
rogers.machara <rogersmachara13@gmail.com>
2024-08-07 08:33:05 +0300

    create pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import reactDom from 'react-dom/client';
+import { createBrowserRouter } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import Partners from './pages/Partners';
+import AboutUs from './AboutUs';
+import SignUp from './pages/SignUp';
+import ErrorPage from './pages/ErrorPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AboutUs />,
+  },
+  {
+    path: '/Partners',
+    element: <Partners />,
+  },
+  {
+    path: '/SignUp',
+    element: <SignUp />,
+  },
+  {
+    path: '/AboutUs',
+    element: <AboutUs />,
+  },
+  {
+    path: '/ErrorPage',
+    element: <ErrorPage />
+  }
+])
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AboutUs />
     </div>
   );
 }
---
+++ client/src/pages/ErrorPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function ErrorPage() {
+  return (
+    <div>
+        404 NOT FOUND
+        <Link></Link>
+    </div>
+  )
+}
+
+export default ErrorPage
